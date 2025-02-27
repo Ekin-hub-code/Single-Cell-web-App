5b19c3cd94927e2f65d2fa64fd3aab08d27ca35b
Ekin-hub-code <ekinmertsoysal@gmail.com>
2025-02-27 15:43:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ import umap
 import numpy as np
 from skylearn.datasets import load_iris
 
-def unzip_file(zip_path, extract_path):
-    #ZIP dosyasını açma
-try:
+ #ZIP dosyasını açma
+def extractzip(zip_path):   
+ try:
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
         print(f"Dosya {extract_path} dizinine çıkarıldı.")
-    return extract_path
+ except Exception as e:
+            return f"ZIP açılırken hata oluştu: {e}"
 
 def load_h5ad(file_path):
     # .h5ad dosyasını yükleme
@@ -21,7 +22,7 @@ def load_h5ad(file_path):
     return adata
 h5ad_files = list(extract_path.glob("*.h5ad"))
 if not h5ad_files:
-    return ".h5ad_files can not be found."
+return ".h5ad_files can not be found."
 
 
 # UI Tanımlama
@@ -29,27 +30,40 @@ app_ui = ui.page_fluid(
     ui.h2("Single cell viwer app"),
     ui.input_file("file_upload", "ZIP dosyası yükle", multiple=False, accept=[".zip"]),
     ui.output_plot("umap_plot"),
-    ui.output_text_verbatim("status")
+    ui.output_text_verbatim("status"),
+    ui.input_action_button("action_button", "Action"),  
+    ui.output_text("counter"),
 )
+app_ui = ui.page_sidebar(  
+    ui.sidebar("Sidebar", position="right", bg="#f8f8f8"),  
+    "Main content",
+)  
 def server(input, output, session):
+    pass
+    @output
+    @render.text()
+    @reactive.event(input.action_button)
+    def counter():
+        return f"{input.action_button()}"
     @reactive.calc
     def process_file():
         file_info = input.file_upload()
         if not file_info:
             return None
-data = load_iris()
-X = data.data
-Y = data.target
-
-#UMAP modelini oluştur
-umap_model = umap.UMAP(n_neighbors=15, min_dist=0.1, n_components=2)
-#Veriyi 2 boyuta indirgeme
-X_umap = umap_model.fit_transform(X)
-#Sonuçları görselleştirme
-plt.scatter(X_umap[:, 0], X_umap[:,1], c=y, cmap='viridis', edgecolors='k', s=50)
-plt.colorbar()
-plt.title('UMAP - Iris veri Kümesi Boyut İndirgeme')
-plt.show()
 
 
+        sc.pp.pca(adata)
+        sc.pp.neighbors(adata, use_rep="X_pca")
+        sc.tl.umap(adata)
+
+        sc.tl.leiden(adata)
+        
+        plt.figure(figsize=(6, 4))
+        sc.pl.umap(adata, color="Cell type", cmap="tab10", alpha=0.5, show=False)
+        
+        return plt.gcf()
+
+
+
+#ShinyApp'i çalıştır
 app = App(app_ui, server)
